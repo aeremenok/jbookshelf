ee9afa8169c45018de2be4613f0fb2635b94bbaf
eav1986 <eav1986@1982285a-9707-11dd-8b10-c13acc454e0b>
2008-10-29 10:24:39 +0000

    added uqeries

--- trunk/projects/JBookShelfModel/src/org/jbookshelf/impl/BookShelfStorageImpl.java
+++ trunk/projects/JBookShelfModel/src/org/jbookshelf/impl/BookShelfStorageImpl.java
@@ -6,10 +6,14 @@
  */
 package org.jbookshelf.impl;
 
+import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EClass;
 
+import org.eclipse.emf.ecore.InternalEObject;
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.eclipse.emf.ecore.impl.EObjectImpl;
 
+import org.jbookshelf.BookShelf;
 import org.jbookshelf.BookShelfStorage;
 import org.jbookshelf.JbookshelfPackage;
 
@@ -18,12 +22,26 @@ import org.jbookshelf.JbookshelfPackage;
  * An implementation of the model object '<em><b>Book Shelf Storage</b></em>'.
  * <!-- end-user-doc -->
  * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link org.jbookshelf.impl.BookShelfStorageImpl#getBookShelf <em>Book Shelf</em>}</li>
+ * </ul>
  * </p>
  *
  * @generated
  */
 public class BookShelfStorageImpl extends EObjectImpl implements BookShelfStorage
 {
+    /**
+     * The cached value of the '{@link #getBookShelf() <em>Book Shelf</em>}' reference.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #getBookShelf()
+     * @generated
+     * @ordered
+     */
+    protected BookShelf bookShelf;
+
     /**
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
@@ -50,6 +68,49 @@ public class BookShelfStorageImpl extends EObjectImpl implements BookShelfStorag
      * <!-- end-user-doc -->
      * @generated
      */
+    public BookShelf getBookShelf()
+    {
+        if (bookShelf != null && bookShelf.eIsProxy())
+        {
+            InternalEObject oldBookShelf = (InternalEObject)bookShelf;
+            bookShelf = (BookShelf)eResolveProxy(oldBookShelf);
+            if (bookShelf != oldBookShelf)
+            {
+                if (eNotificationRequired())
+                    eNotify(new ENotificationImpl(this, Notification.RESOLVE, JbookshelfPackage.BOOK_SHELF_STORAGE__BOOK_SHELF, oldBookShelf, bookShelf));
+            }
+        }
+        return bookShelf;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public BookShelf basicGetBookShelf()
+    {
+        return bookShelf;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public void setBookShelf(BookShelf newBookShelf)
+    {
+        BookShelf oldBookShelf = bookShelf;
+        bookShelf = newBookShelf;
+        if (eNotificationRequired())
+            eNotify(new ENotificationImpl(this, Notification.SET, JbookshelfPackage.BOOK_SHELF_STORAGE__BOOK_SHELF, oldBookShelf, bookShelf));
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
     public void loadCollection()
     {
         // TODO: implement this method
@@ -67,6 +128,73 @@ public class BookShelfStorageImpl extends EObjectImpl implements BookShelfStorag
         // TODO: implement this method
         // Ensure that you remove @generated or mark it @generated NOT
         throw new UnsupportedOperationException();
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public Object eGet(int featureID, boolean resolve, boolean coreType)
+    {
+        switch (featureID)
+        {
+            case JbookshelfPackage.BOOK_SHELF_STORAGE__BOOK_SHELF:
+                if (resolve) return getBookShelf();
+                return basicGetBookShelf();
+        }
+        return super.eGet(featureID, resolve, coreType);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public void eSet(int featureID, Object newValue)
+    {
+        switch (featureID)
+        {
+            case JbookshelfPackage.BOOK_SHELF_STORAGE__BOOK_SHELF:
+                setBookShelf((BookShelf)newValue);
+                return;
+        }
+        super.eSet(featureID, newValue);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public void eUnset(int featureID)
+    {
+        switch (featureID)
+        {
+            case JbookshelfPackage.BOOK_SHELF_STORAGE__BOOK_SHELF:
+                setBookShelf((BookShelf)null);
+                return;
+        }
+        super.eUnset(featureID);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public boolean eIsSet(int featureID)
+    {
+        switch (featureID)
+        {
+            case JbookshelfPackage.BOOK_SHELF_STORAGE__BOOK_SHELF:
+                return bookShelf != null;
+        }
+        return super.eIsSet(featureID);
     }
 
 } //BookShelfStorageImpl
