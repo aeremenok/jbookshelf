462e1164d658747db080e7765acee3b6cd67da9b
eav1986 <eav1986@1982285a-9707-11dd-8b10-c13acc454e0b>
2008-11-05 20:15:58 +0000

    regenerated tests

--- trunk/projects/JBookShelfModel.tests/src/org/jbookshelf/tests/AuthorTest.java
+++ trunk/projects/JBookShelfModel.tests/src/org/jbookshelf/tests/AuthorTest.java
@@ -3,22 +3,13 @@
  */
 package org.jbookshelf.tests;
 
-import junit.framework.Assert;
 import junit.textui.TestRunner;
 
-import org.eclipse.emf.common.util.EList;
 import org.jbookshelf.Author;
-import org.jbookshelf.Comment;
 import org.jbookshelf.JbookshelfFactory;
 
 /**
  * <!-- begin-user-doc --> A test case for the model object '<em><b>Author</b></em>'. <!-- end-user-doc -->
- * <p>
- * The following operations are tested:
- * <ul>
- * <li>{@link org.jbookshelf.Commentable#queryComments(java.lang.String) <em>Query Comments</em>}</li>
- * </ul>
- * </p>
  * 
  * @generated
  */
@@ -46,38 +37,6 @@ public class AuthorTest
         String name )
     {
         super( name );
-    }
-
-    /**
-     * Tests the '{@link org.jbookshelf.Commentable#queryComments(java.lang.String) <em>Query Comments</em>}' operation.
-     * <!-- begin-user-doc --> <!-- end-user-doc -->
-     * 
-     * @see org.jbookshelf.Commentable#queryComments(java.lang.String)
-     * @generated NOT
-     */
-    public void testQueryComments__String()
-    {
-        getFixture().getComments().clear();
-
-        Comment comment1 = JbookshelfFactory.eINSTANCE.createComment();
-        comment1.setTitle( "comment1" );
-        Comment comment2 = JbookshelfFactory.eINSTANCE.createComment();
-        comment2.setTitle( "comment2" );
-
-        getFixture().getComments().add( comment1 );
-        getFixture().getComments().add( comment2 );
-
-        EList<Comment> comments1 = getFixture().queryComments( "comment" );
-        Assert.assertNotNull( comments1 );
-        Assert.assertTrue( comments1.contains( comment1 ) );
-        Assert.assertTrue( comments1.contains( comment2 ) );
-        Assert.assertEquals( comments1.size(), 2 );
-
-        EList<Comment> comments2 = getFixture().queryComments( "comment1" );
-        Assert.assertNotNull( comments2 );
-        Assert.assertTrue( comments2.contains( comment1 ) );
-        Assert.assertFalse( comments2.contains( comment2 ) );
-        Assert.assertEquals( comments2.size(), 1 );
     }
 
     /**
