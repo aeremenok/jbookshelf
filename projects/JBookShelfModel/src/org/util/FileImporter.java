9f1ee945eb10d7c666a88d00c908440335b0eea6
eav1986 <eav1986@1982285a-9707-11dd-8b10-c13acc454e0b>
2008-11-09 17:39:22 +0000

    #8 some improvements in file import

--- projects/JBookShelfModel/src/org/util/FileImporter.java
+++ projects/JBookShelfModel/src/org/util/FileImporter.java
@@ -14,40 +14,82 @@ import org.jbookshelf.ReadingUnit;
 import org.jbookshelf.SingleFile;
 import org.jbookshelf.SingleFileFolder;
 
+/**
+ * imports filesystem into {@link BookShelf}
+ * 
+ * @author eav
+ */
 public class FileImporter
 {
-    public static void importFiles(
-        File[] files,
-        String pattern,
-        BookShelf bookShelf )
+    static class HtmlFolderFileFilter
+        implements
+            FileFilter
     {
-        NameParser nameParser = new NameParser( pattern );
-        for ( File file : files )
-        {
-            PhysicalUnit physicalUnit = createPhysicalUnit( file );
-            nameParser.parse( file.getName() );
+        private String mainFolderName;
 
-            Author author = bookShelf.addAuthor( nameParser.getAuthorName() );
-            System.out.println( author );
-            Category category = bookShelf.addCategory( nameParser.getCategoryName() );
-            System.out.println( category );
-            ReadingUnit book = bookShelf.addReadingUnit( nameParser.getBookName(), author, category, physicalUnit );
-            System.out.println( book );
+        public boolean accept(
+            File pathname )
+        {
+            boolean b = pathname.isDirectory() && pathname.getName().endsWith( "_files" );
+            if ( b )
+            {
+                mainFolderName = pathname.getName();
+            }
+            return b;
         }
+
+        public String getMainFolderName()
+        {
+            return mainFolderName;
+        }
+    }
+
+    public static String cutExtension(
+        File file )
+    {
+        if ( file.isDirectory() )
+        {
+            return file.getName();
+        }
+
+        String name = file.getName();
+        int lastIndexOf = name.lastIndexOf( '.' );
+        if ( name.length() - lastIndexOf > 5 )
+        { // it is probably a part of the name todo which files could have a longer extension?
+            return name;
+        }
+
+        return name.substring( 0, lastIndexOf );
     }
 
     public static void main(
         String[] args )
     {
         BookShelf bookShelf = JbookshelfFactory.eINSTANCE.createBookShelf();
-        File root = new File( "/tmp/test/" );
-        importFiles( root.listFiles(), "%a. %b", bookShelf );
+        File root = new File( "/tmp/test" );
+        new FileImporter()
+        {
+            @Override
+            protected void onImportFailure(
+                File file,
+                Exception e )
+            {
+                System.out.println( "-cannot import file " + file.getAbsolutePath() + " cause:" + e.getMessage() );
+            }
+
+            @Override
+            protected void onImportSuccess(
+                ReadingUnit book )
+            {
+                System.out.println( "+imported " + book.getAuthors().get( 0 ).getName() + ". " + book.getName() );
+            }
+        }.importFiles( root.listFiles(), "%a. %b", bookShelf );
     }
 
     private static PhysicalUnit createPhysicalUnit(
         File file )
     {
-        String fileName = file.getName();
+        String fileName = cutExtension( file );
         if ( file.isDirectory() )
         {
             File[] files = file.listFiles( new FileFilter()
@@ -68,11 +110,24 @@ public class FileImporter
                 return indexFileFolder;
             }
 
-            // simply consider first file a book
-            SingleFileFolder singleFileFolder = JbookshelfFactory.eINSTANCE.createSingleFileFolder();
-            singleFileFolder.setFolder( file );
-            singleFileFolder.setSingleFile( file.listFiles()[0] );
-            return singleFileFolder;
+            File[] listFiles = file.listFiles();
+            if ( listFiles.length == 1 )
+            { // simply single file
+                SingleFileFolder singleFileFolder = JbookshelfFactory.eINSTANCE.createSingleFileFolder();
+                singleFileFolder.setFolder( file );
+                singleFileFolder.setSingleFile( listFiles[0] );
+                return singleFileFolder;
+            }
+
+            File mainFile = getHtmlFile( file );
+            if ( mainFile != null )
+            { // file.html with file_files/
+                SingleFileFolder singleFileFolder = JbookshelfFactory.eINSTANCE.createSingleFileFolder();
+                singleFileFolder.setFolder( file );
+                singleFileFolder.setSingleFile( mainFile );
+                return singleFileFolder;
+            }
+            return null;
         }
 
         // zip file
@@ -87,5 +142,80 @@ public class FileImporter
         SingleFile singleFile = JbookshelfFactory.eINSTANCE.createSingleFile();
         singleFile.setFile( file );
         return singleFile;
+    }
+
+    /**
+     * try to find if folder holds "file" with "file_files/"
+     * 
+     * @param folder folder
+     * @return main file ("file") or null in case failure
+     */
+    private static File getHtmlFile(
+        File folder )
+    {
+        HtmlFolderFileFilter filter = new HtmlFolderFileFilter();
+        File[] listFiles = folder.listFiles( filter );
+        if ( listFiles.length == 1 )
+        {
+            String mainFolderName = filter.getMainFolderName();
+            File mainFile = new File( mainFolderName.substring( 0, mainFolderName.indexOf( "_files" ) ) );
+            if ( mainFile.exists() )
+            {
+                return mainFile;
+            }
+        }
+        return null;
+    }
+
+    public void importFiles(
+        File[] files,
+        String pattern,
+        BookShelf bookShelf )
+    {
+        NameParser nameParser = new NameParser( pattern );
+        for ( File file : files )
+        {
+            PhysicalUnit physicalUnit = createPhysicalUnit( file );
+            if ( physicalUnit != null )
+            {
+                try
+                {
+                    nameParser.parse( cutExtension( file ) );
+
+                    Author author = bookShelf.addAuthor( nameParser.getAuthorName() );
+                    Category category = bookShelf.addCategory( nameParser.getCategoryName() );
+                    ReadingUnit book =
+                        bookShelf.addReadingUnit( nameParser.getBookName(), author, category, physicalUnit );
+
+                    onImportSuccess( book );
+                }
+                catch ( Exception e )
+                {
+                    onImportFailure( file, e );
+                }
+            }
+            else if ( file.isDirectory() )
+            {
+                importFiles( file.listFiles(), pattern, bookShelf );
+            }
+            else
+            {
+                onImportFailure( file, new Exception( "unparseable directory" ) // todo specify more info
+                );
+            }
+        }
+    }
+
+    protected void onImportFailure(
+        @SuppressWarnings( "unused" ) File file,
+        @SuppressWarnings( "unused" ) Exception e )
+    {
+        // override if needed
+    }
+
+    protected void onImportSuccess(
+        @SuppressWarnings( "unused" ) ReadingUnit book )
+    {
+        // override if needed
     }
 }
