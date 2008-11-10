d4eb3756dbc76fc2d36db04d6126a431dc380b74
eav1986 <eav1986@1982285a-9707-11dd-8b10-c13acc454e0b>
2008-11-10 15:51:20 +0000

    #2 settings class

---
+++ projects/JBookShelfModel/src/org/util/settings/Settings.java
@@ -0,0 +1,132 @@
+package org.util.settings;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Properties;
+
+/**
+ * stores settings
+ * 
+ * @author eav
+ */
+public class Settings
+    implements
+        JBookShelfSettings,
+        ThirdPartySettings
+{
+    private static final String DEFAULTS   = "defaults.properties";
+
+    private static Settings     instance;
+
+    private final Properties    properties = new Properties();
+
+    public static Settings getInstance()
+    {
+        if ( instance == null )
+        {
+            instance = new Settings();
+        }
+        return instance;
+    }
+
+    public static void main(
+        String[] args )
+    {
+        getInstance().loadDefaults();
+        System.out.println( getInstance().getProperty( IMPORT_MASK ) );
+        getInstance().setProperty( IMPORT_MASK, "%a====" );
+        System.out.println( getInstance().getProperty( IMPORT_MASK ) );
+    }
+
+    private Settings()
+    {
+        super();
+    }
+
+    public String getProperty(
+        String key )
+    {
+        // todo recreate JBS_FOLDER
+        return properties.getProperty( key );
+    }
+
+    public void load(
+        String file )
+    {
+        try
+        {
+            properties.load( new FileInputStream( file ) );
+        }
+        catch ( Exception e )
+        {
+            throw new Error( e );
+        }
+    }
+
+    public void loadDefaults()
+    {
+        try
+        {
+            load( System.getProperty( "user.dir" ) + File.separator + DEFAULTS );
+        }
+        catch ( Error e )
+        {
+            if ( e.getCause() instanceof FileNotFoundException )
+            {
+                createDefaults();
+                saveAsDefaults();
+            }
+            else
+            {
+                throw e;
+            }
+        }
+    }
+
+    public void save(
+        String fileName,
+        boolean createNewFile )
+    {
+        try
+        {
+            File file = new File( fileName );
+            if ( !file.exists() && createNewFile )
+            {
+                file.createNewFile();
+            }
+            properties.store( new FileOutputStream( file ), "" );
+        }
+        catch ( Exception e )
+        {
+            throw new Error( e );
+        }
+    }
+
+    public void saveAsDefaults()
+    {
+        save( System.getProperty( "user.dir" ) + File.separator + DEFAULTS, true );
+    }
+
+    public Object setProperty(
+        String key,
+        String value )
+    {
+        return properties.setProperty( key, value );
+    }
+
+    private void createDefaults()
+    {
+        properties.clear();
+
+        properties.setProperty( JBookShelfSettings.LANGUAGE, "en" );
+        properties.setProperty( JBookShelfSettings.LAF, "metal" );
+        properties.setProperty( JBookShelfSettings.JBS_FOLDER, System.getProperty( "user.home" ) + File.separator +
+            ".jbookshelf" + File.separator );
+
+        properties.setProperty( JBookShelfSettings.IMPORT_MASK, "%a. %b" );
+        // todo trick windows
+        properties.setProperty( JBookShelfSettings.TEMP_FOLDER, System.getProperty( "java.io.tmpdir" ) );
+    }
+}
