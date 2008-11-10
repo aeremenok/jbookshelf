aa24d0c3b25789b73153ca5286366865a7a11155
eav1986 <eav1986@1982285a-9707-11dd-8b10-c13acc454e0b>
2008-11-10 15:51:20 +0000

    #2 settings class

---
+++ trunk/projects/JBookShelfModel/src/org/util/settings/ThirdPartySettings.java
@@ -0,0 +1,13 @@
+package org.util.settings;
+
+/**
+ * third party proprams setting names
+ * 
+ * @author eav
+ */
+public interface ThirdPartySettings
+{
+    static final String DEFAULT_VIEWER = "default_viewer";
+    static final String OOO_PATH       = "ooo_path";
+    // todo
+}
