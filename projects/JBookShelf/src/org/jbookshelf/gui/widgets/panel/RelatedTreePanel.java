cd105ae20c2a43d282fffcc5676d328a5b214bcc
eav1986 <eav1986@1982285a-9707-11dd-8b10-c13acc454e0b>
2008-11-19 20:17:11 +0000

    #7 panels layout

--- projects/JBookShelf/src/org/jbookshelf/gui/widgets/panel/RelatedTreePanel.java
+++ projects/JBookShelf/src/org/jbookshelf/gui/widgets/panel/RelatedTreePanel.java
@@ -1,7 +1,25 @@
 package org.jbookshelf.gui.widgets.panel;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+
 public class RelatedTreePanel
     extends SearchableTreePanel
 {
+    private javax.swing.JTree       relatedTree = new JTree();
+    private javax.swing.JScrollPane scrollPane  = new JScrollPane( relatedTree );
 
+    public RelatedTreePanel()
+    {
+        super();
+        setLayout( new BorderLayout() );
+        initComponents();
+    }
+
+    protected void initComponents()
+    {
+        add( scrollPane );
+    }
 }
