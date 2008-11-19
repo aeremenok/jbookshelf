0327f8e085952b481045d074dfc5803f22389267
eav1986 <eav1986@1982285a-9707-11dd-8b10-c13acc454e0b>
2008-11-19 15:32:47 +0000

    #7 generic panel class

---
+++ projects/JBookShelf/src/org/jbookshelf/gui/widgets/panel/RelatedTreePanel.java
@@ -0,0 +1,7 @@
+package org.jbookshelf.gui.widgets.panel;
+
+public class RelatedTreePanel
+    extends SearchableTreePanel
+{
+
+}
---
+++ projects/JBookShelf/src/org/jbookshelf/gui/widgets/panel/SearchableTreePanel.java
@@ -0,0 +1,25 @@
+package org.jbookshelf.gui.widgets.panel;
+
+import java.awt.event.KeyEvent;
+
+import javax.swing.JPanel;
+
+public abstract class SearchableTreePanel
+    extends JPanel
+{
+    public void onAdd()
+    {
+        // TODO Auto-generated method stub
+    }
+
+    public void onRemove()
+    {
+        // TODO Auto-generated method stub
+    }
+
+    public void onKeyTyped(
+        KeyEvent evt )
+    {
+        // TODO Auto-generated method stub
+    }
+}
