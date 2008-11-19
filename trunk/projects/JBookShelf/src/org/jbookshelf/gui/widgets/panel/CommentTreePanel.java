b4fdeaf910e2ee1101b9b14f6725270772be5c07
eav1986 <eav1986@1982285a-9707-11dd-8b10-c13acc454e0b>
2008-11-19 20:17:11 +0000

    #7 panels layout

--- trunk/projects/JBookShelf/src/org/jbookshelf/gui/widgets/panel/CommentTreePanel.java
+++ trunk/projects/JBookShelf/src/org/jbookshelf/gui/widgets/panel/CommentTreePanel.java
@@ -1,7 +1,44 @@
 package org.jbookshelf.gui.widgets.panel;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JTree;
+
 public class CommentTreePanel
     extends SearchableTreePanel
 {
+    private JTextArea   commentTextArea    = new JTextArea();
+    private JScrollPane textAreaScrollPane = new JScrollPane( commentTextArea );
+    private JTextField  titleTextField     = new JTextField();
+    private JLabel      dateLabel          = new JLabel();
+    private JTree       commentTree        = new JTree();
+    private JScrollPane treeScrollPane     = new JScrollPane( commentTree );
 
+    public CommentTreePanel()
+    {
+        super();
+        initComponents();
+    }
+
+    protected void initComponents()
+    {
+        // todo
+        String text = "01.01.2008/12-12";
+        dateLabel.setText( text );
+        commentTextArea.setColumns( 20 );
+        commentTextArea.setRows( 5 );
+
+        JPanel panel = new JPanel( new BorderLayout() );
+        add( panel );
+        panel.add( dateLabel, BorderLayout.WEST );
+        panel.add( titleTextField, BorderLayout.CENTER );
+
+        add( textAreaScrollPane );
+        add( treeScrollPane );
+    }
 }
--- trunk/projects/JBookShelf/src/org/jbookshelf/gui/widgets/panel/SearchableTreePanel.java
+++ trunk/projects/JBookShelf/src/org/jbookshelf/gui/widgets/panel/SearchableTreePanel.java
@@ -4,9 +4,16 @@ import java.awt.event.KeyEvent;
 
 import javax.swing.JPanel;
 
+import org.jdesktop.swingx.VerticalLayout;
+
 public abstract class SearchableTreePanel
     extends JPanel
 {
+    public SearchableTreePanel()
+    {
+        super( new VerticalLayout() );
+    }
+
     public void onAdd()
     {
         // TODO Auto-generated method stub
