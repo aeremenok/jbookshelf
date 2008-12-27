2dd13dbab7fde843ce456273bc1c29a42590e2a1
eav1986 <eav1986@1982285a-9707-11dd-8b10-c13acc454e0b>
2008-12-27 12:49:38 +0000

    ant translation target

---
+++ projects/QJBookShelf/translations/jbookshelf.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>org.jbookshelf.qtgui.MainWindow</name>
+    <message>
+        <location filename="../src/org/jbookshelf/qtgui/MainWindow.java" line="41"/>
+        <source>E&amp;xit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/org/jbookshelf/qtgui/MainWindow.java" line="42"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/org/jbookshelf/qtgui/MainWindow.java" line="43"/>
+        <source>Exit the application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/org/jbookshelf/qtgui/MainWindow.java" line="46"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/org/jbookshelf/qtgui/MainWindow.java" line="47"/>
+        <source>Show the application&apos;s About box</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/org/jbookshelf/qtgui/MainWindow.java" line="50"/>
+        <source>About &amp;Qt Jambi</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/org/jbookshelf/qtgui/MainWindow.java" line="51"/>
+        <source>Show the Qt Jambi&apos;s About box</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/org/jbookshelf/qtgui/MainWindow.java" line="57"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/org/jbookshelf/qtgui/MainWindow.java" line="60"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
