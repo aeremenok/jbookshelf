77f54a3b6cecf13b5ce355199d7b0d70a407950b
eav1986 <eav1986@1982285a-9707-11dd-8b10-c13acc454e0b>
2008-12-28 13:44:02 +0000

    commenttreepanel ported

--- projects/QJBookShelf/src/org/jbookshelf/qtgui/widgets/treepanel/CommentTreePanel.java
+++ projects/QJBookShelf/src/org/jbookshelf/qtgui/widgets/treepanel/CommentTreePanel.java
@@ -15,30 +15,42 @@
  */
 package org.jbookshelf.qtgui.widgets.treepanel;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.jbookshelf.model.Comment;
 import org.jbookshelf.model.Commentable;
+import org.jbookshelf.model.ModelFactory;
 import org.jbookshelf.model.Unique;
+import org.jbookshelf.qtgui.JBookShelfConstants;
 import org.jbookshelf.qtgui.widgets.panel.RelatedPanel;
+
+import com.trolltech.qt.gui.QGridLayout;
+import com.trolltech.qt.gui.QIcon;
+import com.trolltech.qt.gui.QLabel;
+import com.trolltech.qt.gui.QLineEdit;
+import com.trolltech.qt.gui.QPlainTextEdit;
+import com.trolltech.qt.gui.QPushButton;
+import com.trolltech.qt.gui.QTreeWidget;
+import com.trolltech.qt.gui.QTreeWidgetItem;
+import com.trolltech.qt.gui.QVBoxLayout;
+import com.trolltech.qt.gui.QWidget;
 
 public class CommentTreePanel
     extends SearchableTreePanel
     implements
-        ActionListener
+        JBookShelfConstants
 {
     private class CommentNode
-    // extends DefaultMutableTreeNode
+        extends QTreeWidgetItem
     {
         private final Comment comment;
 
         public CommentNode(
             Comment comment )
         {
-            // super( comment.getTitle() );
+            setText( 0, comment.getTitle() );
             this.comment = comment;
         }
 
@@ -48,75 +60,82 @@ public class CommentTreePanel
         }
     }
 
-    private static final SimpleDateFormat format = new SimpleDateFormat( "dd.MM.yy HH:mm" );
+    private static final SimpleDateFormat format          = new SimpleDateFormat( "dd.MM.yy HH:mm" );
 
-    // private final DefaultMutableTreeNode root = new DefaultMutableTreeNode();
-    // private final JTree commentTree = new JTree( root );
-    //
-    // private final JTextArea commentTextArea = new JTextArea();
-    // private final JTextField titleTextField = new JTextField();
-    // private final JLabel dateLabel = new JLabel();
-    //
-    // private final JPanel editPanel = new JPanel( new VerticalLayout() )
-    // {
-    // @SuppressWarnings( "synthetic-access" )
-    // @Override
-    // public void setVisible(
-    // boolean flag )
-    // {
-    // super.setVisible( flag );
-    // refresh();
-    // }
-    // };
+    private final QTreeWidgetItem         root            = new QTreeWidgetItem();
+    private final QTreeWidget             commentTree     = new QTreeWidget();
+
+    private final QPlainTextEdit          commentTextArea = new QPlainTextEdit();
+    private final QLineEdit               titleTextField  = new QLineEdit();
+    private final QLabel                  dateLabel       = new QLabel();
+
+    private final QWidget                 editPanel       = new QWidget();
 
     private Commentable                   selectedCommentable;
+
+    private QPushButton                   submitButton    = new QPushButton();
 
     public CommentTreePanel(
         RelatedPanel relatedPanel )
     {
         super( relatedPanel );
         initComponents();
-    }
-
-    public void actionPerformed(
-        ActionEvent e )
-    {
-        // CommentNode node = (CommentNode) commentTree.getLastSelectedPathComponent();
-        // Comment comment = node.getComment();
-        // comment.setContent( commentTextArea.getText() );
-        // comment.setTitle( titleTextField.getText() );
-        //
-        // node.setUserObject( comment.getTitle() );
-        // commentTree.updateUI();
+        initListeners();
     }
 
     public void nothingSelected()
     {
-        // selectedCommentable = null;
-        //
-        // root.removeAllChildren();
-        // commentTree.setRootVisible( false );
-        // commentTree.updateUI();
-        //
-        // editPanel.setVisible( false );
-        // relatedPanel.focusLost();
+        selectedCommentable = null;
+
+        cleanRoot();
+
+        editPanel.setVisible( false );
+        relatedPanel.focusLost();
     }
 
     @Override
     public void onAdd()
     {
-        // Comment comment = ModelFactory.eINSTANCE.createComment();
-        // selectedCommentable.getComments().add( comment );
-        // Date value = new Date();
-        // comment.setCreationDate( value );
-        // comment.setTitle( "comment" + format.format( value ) );
-        // comment.setContent( "" );
-        //
-        // root.add( new CommentNode( comment ) );
-        // commentTree.updateUI();
-        //
-        // commentTree.setSelectionRow( root.getChildCount() - 1 );
-        // editComment( comment );
+        Comment comment = ModelFactory.eINSTANCE.createComment();
+        selectedCommentable.getComments().add( comment );
+        Date value = new Date();
+        comment.setCreationDate( value );
+        comment.setTitle( "comment" + format.format( value ) );
+        comment.setContent( "" );
+
+        root.addChild( new CommentNode( comment ) );
+
+        commentTree.setCurrentItem( root.child( root.childCount() - 1 ) );
+        editComment( comment );
+    }
+
+    @Override
+    public void onRemove()
+    {
+        CommentNode commentNode = (CommentNode) commentTree.selectedItems().get( 0 );
+        selectedCommentable.getComments().remove( commentNode.getComment() );
+
+        int row = root.indexOfChild( commentNode );
+        root.removeChild( commentNode );
+
+        if ( row > 1 )
+        {
+            commentTree.setCurrentItem( root.child( row - 1 ) );
+        }
+        else
+        {
+            relatedPanel.focusLost();
+        }
+    }
+
+    public void saveComment()
+    {
+        CommentNode node = (CommentNode) commentTree.selectedItems().get( 0 );
+        Comment comment = node.getComment();
+        comment.setContent( commentTextArea.toPlainText() );
+        comment.setTitle( titleTextField.text() );
+
+        node.setText( 0, comment.getTitle() );
     }
 
     @Override
@@ -133,104 +152,81 @@ public class CommentTreePanel
         }
     }
 
-    @Override
-    public void onRemove()
-    {
-        // CommentNode commentNode = (CommentNode) commentTree.getLastSelectedPathComponent();
-        // selectedCommentable.getComments().remove( commentNode.getComment() );
-        //
-        // int row = root.getIndex( commentNode );
-        // root.remove( commentNode );
-        // commentTree.updateUI();
-        //
-        // if ( row > 1 )
-        // {
-        // commentTree.setSelectionRow( row - 1 );
-        // }
-        // else
-        // {
-        // relatedPanel.focusLost();
-        // }
-    }
-
     public void selectedUnique(
         Unique unique )
     {
-        // editPanel.setVisible( false );
-        // relatedPanel.focusLost();
-        //
-        // selectedCommentable = (Commentable) unique;
-        // drawComments( selectedCommentable.getComments() );
+        editPanel.setVisible( false );
+        relatedPanel.focusLost();
+
+        selectedCommentable = (Commentable) unique;
+        drawComments( selectedCommentable.getComments() );
+    }
+
+    private void cleanRoot()
+    {
+        for ( int i = 0; i < root.childCount(); i++ )
+        {
+            root.removeChild( root.child( i ) );
+        }
     }
 
     private void drawComments(
         List<Comment> comments )
     {
-        // root.removeAllChildren();
-        // commentTree.setRootVisible( true );
-        //
-        // for ( Comment comment : comments )
-        // {
-        // root.add( new CommentNode( comment ) );
-        // }
-        //
-        // commentTree.expandRow( 0 );
-        // commentTree.setRootVisible( false );
-        //
-        // commentTree.updateUI();
+        cleanRoot();
+        for ( Comment comment : comments )
+        {
+            root.addChild( new CommentNode( comment ) );
+        }
+        root.setExpanded( true );
     }
 
     private void editComment(
         Comment comment )
     {
-        // dateLabel.setText( "Created " + format.format( comment.getCreationDate() ) );
-        // titleTextField.setText( comment.getTitle() );
-        // commentTextArea.setText( comment.getContent() );
-        //
-        // editPanel.setVisible( true );
+        dateLabel.setText( tr( "Created " ) + format.format( comment.getCreationDate() ) );
+        titleTextField.setText( comment.getTitle() );
+        commentTextArea.setPlainText( comment.getContent() );
+
+        editPanel.setVisible( true );
     }
 
     private void initComponents()
     {
-        // JButton submitButton = new JButton();
-        // submitButton.addActionListener( this );
-        // submitButton.setIcon( createIcon( "/org/jbookshelf/gui/images/dialog-ok-apply.png" ) );
-        //
-        // JPanel headerPanel = new JPanel( new BorderLayout() );
-        // headerPanel.add( dateLabel, BorderLayout.WEST );
-        // headerPanel.add( titleTextField, BorderLayout.CENTER );
-        // headerPanel.add( submitButton, BorderLayout.EAST );
-        //
-        // commentTextArea.setColumns( 20 );
-        // commentTextArea.setRows( 5 );
-        //
-        // editPanel.add( headerPanel );
-        // editPanel.add( new JScrollPane( commentTextArea ) );
-        //
-        // add( editPanel, BorderLayout.NORTH );
-        // add( new JScrollPane( commentTree ), BorderLayout.CENTER );
-        //
-        // commentTree.addTreeSelectionListener( new TreeSelectionListener()
-        // {
-        // @SuppressWarnings( "synthetic-access" )
-        // public void valueChanged(
-        // TreeSelectionEvent e )
-        // {
-        // CommentNode commentNode = (CommentNode) commentTree.getLastSelectedPathComponent();
-        // if ( commentNode != null )
-        // {
-        // editComment( commentNode.getComment() );
-        // }
-        // relatedPanel.focusGained();
-        // }
-        // } );
-        //
-        // nothingSelected();
+        commentTree.addTopLevelItem( root );
+        commentTree.header().hide();
+
+        QGridLayout editLayout = new QGridLayout();
+        editPanel.setLayout( editLayout );
+        editLayout.addWidget( dateLabel, 0, 0 );
+        editLayout.addWidget( titleTextField, 0, 1 );
+        editLayout.addWidget( submitButton, 0, 2 );
+
+        editLayout.addWidget( commentTextArea, 1, 0, 1, 3 );
+
+        setLayout( new QVBoxLayout() );
+        layout().addWidget( editPanel );
+        layout().addWidget( commentTree );
+
+        submitButton.setIcon( new QIcon( ICONPATH + "dialog-ok-apply.png" ) );
+
+        nothingSelected();
     }
 
-    private void refresh()
+    private void initListeners()
     {
-        // updateUI();
-        // editPanel.updateUI();
+        commentTree.itemSelectionChanged.connect( this, "onItemSelection()" );
+        submitButton.released.connect( this, "saveComment()" );
+    }
+
+    @SuppressWarnings( "unused" )
+    private void onItemSelection()
+    {
+        CommentNode commentNode = (CommentNode) commentTree.selectedItems().get( 0 );
+        if ( commentNode != null )
+        {
+            editComment( commentNode.getComment() );
+        }
+        relatedPanel.focusGained();
     }
 }
