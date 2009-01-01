ac466a6860b10b0e3cd43881734fcaa486e48a2f
eav1986 <eav1986@1982285a-9707-11dd-8b10-c13acc454e0b>
2009-01-01 16:17:16 +0000

    #24 dynamically switching translations (only once %) )

--- projects/QJBookShelf/src/org/jbookshelf/qtgui/logic/Translator.java
+++ projects/QJBookShelf/src/org/jbookshelf/qtgui/logic/Translator.java
@@ -3,9 +3,14 @@ package org.jbookshelf.qtgui.logic;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.trolltech.qt.core.QTranslator;
+import com.trolltech.qt.gui.QApplication;
+
 public class Translator
 {
     private static final List<Translatable> translatables = new ArrayList<Translatable>();
+
+    private static QTranslator              translator;
 
     public static void addTranslatable(
         Translatable translatable )
@@ -21,14 +26,22 @@ public class Translator
     }
 
     public static void retranslate(
-        @SuppressWarnings( "unused" ) String language )
+        String language )
     {
-        // todo switch translation
-        // QApplication.installTranslator( language );
+        if ( "Russian".equals( language ) )
+        {
+            translator = new QTranslator();
+            QApplication.installTranslator( translator );
+            translator.load( "classpath:/../translations/jbookshelf.qm" );
+        }
+        else if ( translator != null )
+        {
+            QApplication.removeTranslator( translator );
+        }
+
         for ( Translatable translatable : translatables )
         {
             translatable.retranslate();
         }
     }
-
 }
