f3fe5e8edeea541a7ea459b29d1242849e8e46f8
eav1986 <eav1986@1982285a-9707-11dd-8b10-c13acc454e0b>
2009-01-02 16:03:47 +0000

    directories reorganized

--- projects/QJBookShelf/src/org/jbookshelf/qtgui/logic/JBookShelfConstants.java
+++ projects/QJBookShelf/src/org/jbookshelf/qtgui/logic/JBookShelfConstants.java
@@ -2,5 +2,5 @@ package org.jbookshelf.qtgui.logic;
 
 public interface JBookShelfConstants
 {
-    String ICONPATH = "classpath:/org/jbookshelf/qtgui/images/";
+    String ICONPATH = "classpath:/images/";
 }
--- projects/QJBookShelf/src/org/jbookshelf/qtgui/logic/Translator.java
+++ projects/QJBookShelf/src/org/jbookshelf/qtgui/logic/Translator.java
@@ -9,19 +9,20 @@ public class Translator
 {
     private static final List<Translatable> translatables = new ArrayList<Translatable>();
 
+    private static QTranslator              currentTranslator;
+
     public static final QTranslator         RU;
     public static final QTranslator         EN;
 
-    private static QTranslator              translator;
     static
     {
         RU = new QTranslator();
-        RU.load( "classpath:/../translations/jbookshelf_ru.qm" );
+        RU.load( "classpath:/translations/jbookshelf_ru.qm" );
 
         EN = new QTranslator();
-        EN.load( "classpath:/../translations/jbookshelf_en.qm" );
+        EN.load( "classpath:/translations/jbookshelf_en.qm" );
 
-        translator = EN;
+        currentTranslator = EN;
     }
 
     public static void addTranslatable(
@@ -40,7 +41,7 @@ public class Translator
     public static void retranslate(
         String language )
     {
-        translator = "Russian".equals( language ) ? RU : EN;
+        currentTranslator = "Russian".equals( language ) ? RU : EN;
         for ( Translatable translatable : translatables )
         {
             translatable.retranslate();
@@ -51,7 +52,7 @@ public class Translator
         String context,
         String source )
     {
-        String translate = translator.translate( context, source );
+        String translate = currentTranslator.translate( context, source );
         if ( translate.equals( "" ) )
         {
             return source;
