1520a2c19e18b8d3c529a99b8a6ed54f1f31fa97
eav1986 <eav1986@1982285a-9707-11dd-8b10-c13acc454e0b>
2009-01-24 12:58:26 +0000

    complie error in unit test

--- trunk/projects/JBookShelfModel.tests/src/org/jbookshelf/tests/NameParserTest.java
+++ trunk/projects/JBookShelfModel.tests/src/org/jbookshelf/tests/NameParserTest.java
@@ -25,20 +25,27 @@ public class NameParserTest
 {
     public final void testParse()
     {
-        NameParser nameParser = new NameParser( "[%a]-%b    %c." );
-        nameParser.parse( "[a]-b    c." );
-        Assert.assertEquals( "a", nameParser.getAuthorName() );
-        Assert.assertEquals( "b", nameParser.getBookName() );
-        Assert.assertEquals( "c", nameParser.getCategoryName() );
-        nameParser.parse( "[Author]-%Book    ]-Category." );
-        Assert.assertEquals( "Author", nameParser.getAuthorName() );
-        Assert.assertEquals( "%Book", nameParser.getBookName() );
-        Assert.assertEquals( "]-Category", nameParser.getCategoryName() );
+        try
+        {
+            NameParser nameParser = new NameParser( "[%a]-%b    %c." );
+            nameParser.parse( "[a]-b    c." );
+            Assert.assertEquals( "a", nameParser.getAuthorName() );
+            Assert.assertEquals( "b", nameParser.getBookName() );
+            Assert.assertEquals( "c", nameParser.getCategoryName() );
+            nameParser.parse( "[Author]-%Book    ]-Category." );
+            Assert.assertEquals( "Author", nameParser.getAuthorName() );
+            Assert.assertEquals( "%Book", nameParser.getBookName() );
+            Assert.assertEquals( "]-Category", nameParser.getCategoryName() );
 
-        NameParser nameParser1 = new NameParser( "%a %b %c" );
-        nameParser1.parse( "Author Book Category" );
-        Assert.assertEquals( "Author", nameParser1.getAuthorName() );
-        Assert.assertEquals( "Book", nameParser1.getBookName() );
-        Assert.assertEquals( "Category", nameParser1.getCategoryName() );
+            NameParser nameParser1 = new NameParser( "%a %b %c" );
+            nameParser1.parse( "Author Book Category" );
+            Assert.assertEquals( "Author", nameParser1.getAuthorName() );
+            Assert.assertEquals( "Book", nameParser1.getBookName() );
+            Assert.assertEquals( "Category", nameParser1.getCategoryName() );
+        }
+        catch ( Exception e )
+        {
+            fail();
+        }
     }
 }
