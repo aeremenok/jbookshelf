8df890e82368aa8a4b2aef057be74b106355bfb1
eav1986 <eav1986@1982285a-9707-11dd-8b10-c13acc454e0b>
2009-05-05 17:07:47 +0000

    #81

--- trunk/projects/QJBookShelf/src/org/jbookshelf/swinggui/widgets/panel/BookPanel.java
+++ trunk/projects/QJBookShelf/src/org/jbookshelf/swinggui/widgets/panel/BookPanel.java
@@ -1,81 +1,27 @@
 package org.jbookshelf.swinggui.widgets.panel;
 
-import java.awt.BorderLayout;
-import java.io.File;
+import java.awt.GridBagLayout;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import org.jbookshelf.model.Book;
 
 public class BookPanel
     extends JPanel
 {
-    /**
-     * Parameter set to excange between classes
-     * 
-     * @author eav
-     */
-    public class Parameters
-    {
-        private final String   bookName;
-        private final String[] authorNames;
-        private final String[] categoryNames;
-        private final File     file;
-        private final boolean  isRead;
-        private final String   viewer;
+    private final JLabel     bookLabel     = new JLabel();
+    private final JLabel     authorLabel   = new JLabel();
+    private final JLabel     categoryLabel = new JLabel();
+    private final JLabel     viewerLabel   = new JLabel();
+    private final JLabel     fileLabel     = new JLabel();
 
-        public Parameters(
-            final String bookName,
-            final String[] authorNames,
-            final String[] categoryNames,
-            final File file,
-            final boolean isRead,
-            final String viewer )
-        {
-            super();
-            this.bookName = bookName;
-            this.authorNames = authorNames;
-            this.categoryNames = categoryNames;
-            this.file = file;
-            this.isRead = isRead;
-            this.viewer = viewer;
-        }
-
-        public String[] getAuthorNames()
-        {
-            return authorNames;
-        }
-
-        public String getBookName()
-        {
-            return bookName;
-        }
-
-        public String[] getCategoryNames()
-        {
-            return categoryNames;
-        }
-
-        public File getFile()
-        {
-            return file;
-        }
-
-        public String getViewer()
-        {
-            return viewer;
-        }
-
-        public boolean isRead()
-        {
-            return isRead;
-        }
-    }
+    private final JTextField bookTextField = new JTextField();
 
     public static void changeBook(
         final Book book,
-        final Parameters parameters )
+        final BookParameters parameters )
     {
         // TODO Auto-generated method stub
 
@@ -83,19 +29,17 @@ public class BookPanel
 
     public BookPanel()
     {
-        super( new BorderLayout() );
-        // todo
-        add( new JLabel( "777" ), BorderLayout.CENTER );
+        super( new GridBagLayout() );
+
     }
 
     public void clear(
         final boolean b )
     {
         // TODO Auto-generated method stub
-
     }
 
-    public Parameters extractParameters()
+    public BookParameters extractParameters()
     {
         // TODO Auto-generated method stub
         return null;
