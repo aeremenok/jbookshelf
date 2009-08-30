5901899bcedb33eea7256d7149cc99f27ce994fe
eav1986 <eav1986@1982285a-9707-11dd-8b10-c13acc454e0b>
2009-08-30 19:43:32 +0000

    #108 started

--- trunk/projects/JBookShelfReader/src/org/jbookshelf/view/swinggui/reader/textpanel/navigate/BookmarkPanel.java
+++ trunk/projects/JBookShelfReader/src/org/jbookshelf/view/swinggui/reader/textpanel/navigate/BookmarkPanel.java
@@ -3,10 +3,33 @@
  */
 package org.jbookshelf.view.swinggui.reader.textpanel.navigate;
 
-import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableModel;
 
 import org.apache.log4j.Logger;
+import org.bushe.swing.event.annotation.AnnotationProcessor;
+import org.bushe.swing.event.annotation.EventSubscriber;
+import org.jbookshelf.controller.singleton.Single;
+import org.jbookshelf.model.db.Book;
+import org.jbookshelf.model.db.Note;
+import org.jbookshelf.model.db.util.BookShelf;
+import org.jbookshelf.view.i18n.I18N;
+import org.jbookshelf.view.logic.SafeWorker;
+import org.jbookshelf.view.swinggui.ProgressBar;
+import org.jbookshelf.view.swinggui.reader.ReaderWindow;
+import org.jbookshelf.view.swinggui.reader.textpanel.LayoutablePanel;
+import org.jbookshelf.view.swinggui.reader.toolbar.Paginator;
+import org.jdesktop.swingx.JXTable;
+import org.jdesktop.swingx.decorator.SortOrder;
 
 /**
  * @author eav 2009
@@ -14,13 +37,136 @@ import org.apache.log4j.Logger;
 public class BookmarkPanel
     extends JPanel
 {
+    public static class BoookmarkTableModel
+        extends DefaultTableModel
+    {
+        private static String[]  names =
+                                       { "#", I18N.tr( "Title" ), I18N.tr( "Pos" ) };
+
+        private final List<Note> notes = new ArrayList<Note>();
+
+        public BoookmarkTableModel()
+        {
+            super( names, 0 );
+            AnnotationProcessor.process( this );
+        }
+
+        public Note getNote(
+            final int row )
+        {
+            return notes.get( row );
+        }
+
+        @Override
+        public int getRowCount()
+        {
+            return notes != null
+                ? notes.size() : 0;
+        }
+
+        @Override
+        public Object getValueAt(
+            final int row,
+            final int column )
+        {
+            final Note note = notes.get( row );
+            switch ( column )
+            {
+                case 0:
+                    return note.getId();
+                case 1:
+                    return note.getTitle();
+                case 2:
+                    final String pos = note.getPosition().toString();
+                    if ( pos.length() > 4 )
+                    {
+                        return pos.substring( 0, 4 );
+                    }
+                    return pos;
+            }
+            return super.getValueAt( row, column );
+        }
+
+        @Override
+        public boolean isCellEditable(
+            final int row,
+            final int column )
+        {
+            return false;
+        }
+
+        @EventSubscriber( eventClass = Note.class )
+        public void update(
+            @SuppressWarnings( "unused" ) final Note note )
+        {
+            Single.instance( ProgressBar.class ).invoke( new SafeWorker<Object, Object>()
+            {
+                @Override
+                protected Object doInBackground()
+                {
+                    final Book book = Single.instance( ReaderWindow.class ).getBook();
+                    notes.clear();
+                    notes.addAll( BookShelf.getNotes( book ) );
+                    return null;
+                }
+
+                @Override
+                protected void doneSafe()
+                {
+                    fireTableDataChanged();
+                }
+            } );
+        }
+    }
+
     @SuppressWarnings( "unused" )
-    private static final Logger log = Logger.getLogger( BookmarkPanel.class );
+    private static final Logger       log   = Logger.getLogger( BookmarkPanel.class );
+    private final BoookmarkTableModel model = new BoookmarkTableModel();
+    private final JXTable             table = new JXTable( model );
 
     public BookmarkPanel()
     {
-        super();
-        // todo 
-        add( new JButton( "bookmarks" ) );
+        super( new BorderLayout() );
+        add( new JScrollPane( table ), BorderLayout.CENTER );
+        model.update( null );
+
+        table.setSortOrder( 0, SortOrder.ASCENDING );
+
+        // todo resize automatically
+        final Dimension mainSize = Single.instance( LayoutablePanel.class ).getCurrentPanels().getSize();
+        final Dimension preferredScrollableViewportSize = table.getPreferredScrollableViewportSize();
+        final Dimension size = new Dimension( preferredScrollableViewportSize.width + 30, mainSize.height - 45 );
+        table.setPreferredScrollableViewportSize( size );
+
+        table.packAll();
+
+        table.setSelectionMode( ListSelectionModel.SINGLE_SELECTION );
+        table.addMouseListener( new MouseAdapter()
+        {
+            @Override
+            public void mouseClicked(
+                final MouseEvent e )
+            {
+                if ( e.getClickCount() == 2 )
+                {
+                    final int rowAtPoint = table.rowAtPoint( e.getPoint() );
+                    if ( rowAtPoint > -1 )
+                    {
+                        final int selectedRow = table.convertRowIndexToModel( rowAtPoint );
+                        final Note note = model.getNote( selectedRow );
+                        final Integer page = note.getPage();
+                        if ( page != null && note.getPageCount() > 1 )
+                        {
+                            Single.instance( Paginator.class ).setNewPage( page );
+                        }
+                        else
+                        {
+                            @SuppressWarnings( "unused" ) final Float position = note.getPosition();
+                            // todo
+                        }
+                    }
+                }
+            }
+        } );
     }
 }
