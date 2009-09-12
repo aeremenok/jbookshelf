890aa94b262db847b44da3318dab398eae28447f
eav1986 <eav1986@1982285a-9707-11dd-8b10-c13acc454e0b>
2009-09-12 18:28:07 +0000

    #62 continued

--- trunk/projects/JBookShelfReader/src/org/jbookshelf/view/swinggui/reader/types/html/ProxyUserAgentContext.java
+++ trunk/projects/JBookShelfReader/src/org/jbookshelf/view/swinggui/reader/types/html/ProxyUserAgentContext.java
@@ -8,11 +8,18 @@ import java.net.InetSocketAddress;
 import java.net.PasswordAuthentication;
 import java.net.Proxy;
 import java.net.SocketAddress;
+import java.net.URL;
+import java.net.URLConnection;
 import java.net.Proxy.Type;
 
 import org.apache.log4j.Logger;
 import org.jbookshelf.controller.settings.Settings;
 import org.jbookshelf.controller.singleton.Single;
+import org.jbookshelf.view.i18n.I18N;
+import org.jbookshelf.view.swinggui.reader.ReaderWindow;
+import org.jdesktop.swingx.JXLoginDialog;
+import org.jdesktop.swingx.JXLoginPane;
+import org.jdesktop.swingx.auth.LoginService;
 import org.lobobrowser.html.test.SimpleUserAgentContext;
 
 /**
@@ -24,22 +31,87 @@ public class ProxyUserAgentContext
     private class AskUserAuthenticator
         extends Authenticator
     {
-        private final String user = System.getProperty( "user.name" );
-        private final String pass = null;
+        private String user = System.getProperty( "user.name" );
+        private char[] pass = null;
 
         @Override
         protected PasswordAuthentication getPasswordAuthentication()
         {
             if ( pass == null )
             {
-                // todo init
+                final CheckCredentialsService service = new CheckCredentialsService();
+                final JXLoginDialog dialog = new JXLoginDialog( Single.instance( ReaderWindow.class ), true );
+                dialog.setTitle( I18N.tr( "Credentials are not stored anywhere" ) );
+
+                final JXLoginPane panel = dialog.getPanel();
+                panel.setLoginService( service );
+                panel.setUserName( user );
+
+                dialog.setVisible( true );
+
+                if ( service.getPassword() == null )
+                {
+                    return null;
+                }
+                user = service.getName();
+                pass = service.getPassword();
             }
-            return new PasswordAuthentication( user, pass.toCharArray() );
+            return new PasswordAuthentication( user, pass );
         }
     }
 
-    @SuppressWarnings( "unused" )
-    private static final Logger log = Logger.getLogger( ProxyUserAgentContext.class );
+    private class CheckCredentialsService
+        extends LoginService
+    {
+        private String name;
+        private char[] password;
+
+        @Override
+        public boolean authenticate(
+            final String name,
+            final char[] password,
+            final String server )
+        {
+            try
+            {
+                Authenticator.setDefault( new Authenticator()
+                {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication()
+                    {
+                        return new PasswordAuthentication( name, password );
+                    }
+                } );
+                final URLConnection connection = new URL( EventRendererContext.GOOGLE_URL ).openConnection( getProxy() );
+                connection.connect();
+
+                this.name = name;
+                this.password = password;
+
+                Authenticator.setDefault( askUserAuthenticator );
+
+                return true;
+            }
+            catch ( final Exception e )
+            {
+                log.error( e, e );
+                return false;
+            }
+        }
+
+        public String getName()
+        {
+            return this.name;
+        }
+
+        public char[] getPassword()
+        {
+            return this.password;
+        }
+    }
+
+    private static final Logger        log                  = Logger.getLogger( ProxyUserAgentContext.class );
+    private final AskUserAuthenticator askUserAuthenticator = new AskUserAuthenticator();
 
     public ProxyUserAgentContext()
     {
@@ -56,7 +128,7 @@ public class ProxyUserAgentContext
             this.setProxy( new Proxy( type, sa ) );
         }
 
-        Authenticator.setDefault( new AskUserAuthenticator() );
+        Authenticator.setDefault( askUserAuthenticator );
     }
 
 }
