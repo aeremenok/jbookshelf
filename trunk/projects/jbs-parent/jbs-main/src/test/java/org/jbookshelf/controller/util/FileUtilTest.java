6df401ef08382daae23a7e22ffd36593c5df98c8
eav1986 <eav1986@1982285a-9707-11dd-8b10-c13acc454e0b>
2010-02-21 17:31:44 +0000

    encoding detection test

---
+++ trunk/projects/jbs-parent/jbs-main/src/test/java/org/jbookshelf/controller/util/FileUtilTest.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package org.jbookshelf.controller.util;
+
+import static org.jbookshelf.controller.singleton.Single.instance;
+import static org.testng.Assert.assertEquals;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.log4j.Logger;
+import org.jbookshelf.TestEnvironment;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * @author eav 2010
+ */
+public class FileUtilTest
+{
+    @SuppressWarnings( "unused" )
+    private static final Logger log = Logger.getLogger( FileUtilTest.class );
+
+    @BeforeClass
+    public void setUp()
+    {
+        instance( TestEnvironment.class ).setUp();
+    }
+
+    @SuppressWarnings( "unchecked" )
+    @DataProvider
+    public Iterator encodedFiles()
+        throws URISyntaxException
+    {
+        final File dir = new File( getClass().getResource( "./test-files/" ).toURI() );
+        final Collection<File> listFiles = FileUtils.listFiles( dir, new String[]
+        { "txt" }, false );
+        final List<Object[]> list = new ArrayList<Object[]>();
+        for ( final File file : listFiles )
+        {
+            list.add( new Object[]
+            { file, FilenameUtils.getBaseName( file.getName() ) } );
+        }
+        return list.iterator();
+    }
+
+    @Test( dataProvider = "encodedFiles" )
+    public void guessFileEncoding(
+        final File file,
+        final String expectedEncoding )
+    {
+        final String encoding = FileUtil.guessFileEncoding( file );
+        assertEquals( Charset.forName( encoding ), Charset.forName( expectedEncoding ) );
+    }
+
+    @AfterClass
+    public void tearDown()
+    {
+        instance( TestEnvironment.class ).tearDown();
+    }
+}
